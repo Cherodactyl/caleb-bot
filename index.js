29202b17339d21b247626f5b5166c33b57c16cb8
Cher Hime <dear.hime@icloud.com>
2025-04-17 00:16:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -187,4 +187,4 @@ if (message.content.toLowerCase().includes("zayne")) {
   if (message.system || (message.author.bot && message.author.id === client.user.id)) return;
 });
 
-client.login(process.env.DISCORD_TOKEN););
+client.login(process.env.DISCORD_TOKEN);
